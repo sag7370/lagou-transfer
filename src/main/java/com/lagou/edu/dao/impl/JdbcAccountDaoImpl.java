44606b63b88378b2d881ee9eb17a4c14115cdcf2
SAg <sag7370@163.com>
2023-08-21 09:50:50 +0800

    add: 注入bean时，使用构造器（index,name）,注入集合

--- src/main/java/com/lagou/edu/dao/impl/JdbcAccountDaoImpl.java
+++ src/main/java/com/lagou/edu/dao/impl/JdbcAccountDaoImpl.java
@@ -9,6 +9,10 @@ import com.lagou.edu.utils.DruidUtils;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 
 /**
  * @author 应癫
@@ -19,6 +23,55 @@ public class JdbcAccountDaoImpl implements AccountDao {
 
     public void setConnectionUtils(ConnectionUtils connectionUtils) {
         this.connectionUtils = connectionUtils;
+    }
+
+    public JdbcAccountDaoImpl(ConnectionUtils connectionUtils, String name, int sex, float money) {
+        this.connectionUtils = connectionUtils;
+        this.name = name;
+        this.sex = sex;
+        this.money = money;
+    }
+
+    private String name;
+    private int sex;
+    private float money;
+
+    private String[] myArray;
+    private Map<String,String> myMap;
+    private Set<String> mySet;
+    private Properties myProperties;
+    private List<String> myList;
+
+    public void setMyList(List<String> myList) {
+        this.myList = myList;
+    }
+
+    public void setMyArray(String[] myArray) {
+        this.myArray = myArray;
+    }
+
+    public void setMyMap(Map<String, String> myMap) {
+        this.myMap = myMap;
+    }
+
+    public void setMySet(Set<String> mySet) {
+        this.mySet = mySet;
+    }
+
+    public void setMyProperties(Properties myProperties) {
+        this.myProperties = myProperties;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public void setMoney(float money) {
+        this.money = money;
     }
 
     /**
